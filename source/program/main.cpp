26d2207f0cb07ab0584ec057c5d5ab16f9c94690
keikei14 <46136089+keikei14@users.noreply.github.com>
2023-11-17 21:27:10 +0800

    Temp commit (a bitch is tired)
    This currently crashes MM. L62 is what crashes it. On Ryujinx, it crashes with an access violation.
    I am just saving my code here.

--- source/program/main.cpp
+++ source/program/main.cpp
@@ -1,39 +1,76 @@
 #include "lib.hpp"
+#include <string>
+#include <vector>
+#include <cstdlib>
 
-/* Define hook StubCopyright. Trampoline indicates the original function should be kept. */
-/* HOOK_DEFINE_REPLACE can be used if the original function does not need to be kept. */
-HOOK_DEFINE_TRAMPOLINE(StubCopyright) {
+void* (*customNew)(size_t) = reinterpret_cast<void*(*)(size_t)>(exl::util::modules::GetTargetStart() + 0x9f87c0);
+void (*customDelete)(void*) = reinterpret_cast<void(*)(void*)>(exl::util::modules::GetTargetStart() + 0x9f87a0);
 
+void* operator new(size_t size) {
+    void* ptr = customNew(size);
+    return ptr;
+}
+
+void operator delete(void* ptr) noexcept {
+    customDelete(ptr);
+}
+
+HOOK_DEFINE_TRAMPOLINE(InitMdataMgr) {
     /* Define the callback for when the function is called. Don't forget to make it static and name it Callback. */
-    static void Callback(bool enabled) {
-
-        /* Call the original function, with the argument always being false. */
-        Orig(false);
+    static void Callback(u64 a1) {
+        Orig(a1);
     }
 
 };
 
+void mainPatch() {
 
-/* Declare function to dynamic link with. */
-namespace nn::oe {
-    void SetCopyrightVisibility(bool);
-};
+    namespace patch = exl::patch;
+    namespace inst = exl::armv8::inst;
+    namespace reg = exl::armv8::reg;
+
+    std::vector<std::string*> *stringPointers = new std::vector<std::string*>;
+
+    stringPointers->push_back(new std::string("mdata_"));
+    stringPointers->push_back(new std::string("end_"));
+    stringPointers->push_back(new std::string("patch_"));
+    stringPointers->push_back(new std::string("patch2_"));
+    stringPointers->push_back(new std::string("dlc_patch_"));
+    stringPointers->push_back(new std::string("dlc1_"));
+    stringPointers->push_back(new std::string("dlc2A_"));
+    stringPointers->push_back(new std::string("dlc2B_"));
+    stringPointers->push_back(new std::string("dlc3A_"));
+    stringPointers->push_back(new std::string("dlc3B_"));
+    stringPointers->push_back(new std::string("dlc4_"));
+    stringPointers->push_back(new std::string("privilege_"));
+    stringPointers->push_back(new std::string("dlc7_"));
+    stringPointers->push_back(new std::string("dlc8_"));
+    stringPointers->push_back(new std::string("dlc9_"));
+    stringPointers->push_back(new std::string("dlc10_"));
+    stringPointers->push_back(new std::string("dlc11_"));
+    stringPointers->push_back(new std::string("dlc12_"));
+    stringPointers->push_back(new std::string("dlc13_"));
+    stringPointers->push_back(new std::string("dlc14_"));
+    stringPointers->push_back(new std::string("dlc15_"));
+    stringPointers->push_back(new std::string("dlc16_"));
+    stringPointers->push_back(new std::string("dlc17_"));
+    stringPointers->push_back(new std::string("dlc18_"));
+    stringPointers->push_back(new std::string("dlc19_"));
+    stringPointers->push_back(new std::string("xpack_"));
+
+    patch::CodePatcher p(0x41af38);
+    p.WriteInst(inst::Adr(reg::X25, (uintptr_t)&stringPointers));
+    p.Seek(0x41af3c);
+    p.WriteInst(inst::Nop());
+    p.Seek(0x41b05c);
+    p.WriteInst(inst::CmpImmediate(reg::X27, stringPointers->size()));
+}
 
 extern "C" void exl_main(void* x0, void* x1) {
-    /* Setup hooking enviroment. */
     exl::hook::Initialize();
 
-    /* Install the hook at the provided function pointer. Function type is checked against the callback function. */
-    StubCopyright::InstallAtFuncPtr(nn::oe::SetCopyrightVisibility);
-
-    /* Alternative install funcs: */
-    /* InstallAtPtr takes an absolute address as a uintptr_t. */
-    /* InstallAtOffset takes an offset into the main module. */
-
-    /*
-    For sysmodules/applets, you have to call the entrypoint when ready
-    exl::hook::CallTargetEntrypoint(x0, x1);
-    */
+    mainPatch();
+    //InitMdataMgr::InstallAtOffset(0x41aec0);
 }
 
 extern "C" NORETURN void exl_exception_entry() {
